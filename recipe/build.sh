029d892099d9c1a7c9f3b5412e0ee59bc8211d57
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-07-15 12:38:12 -0400

    Use `pip` to install
    Make sure that `pip` is used to install `pyflann`. This helps `pip` to
    recognize that this requirement has already been satisfied when
    installing other things.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,4 @@
 # python source is bundled with flann source
 cd src/python
 cmake . -DLIBRARY_OUTPUT_PATH=$PREFIX/lib -DFLANN_VERSION="$PKG_VERSION"
-$PYTHON setup.py install
+$PYTHON -m pip install --no-deps --ignore-installed .
